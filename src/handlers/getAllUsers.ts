e63fb6847236cd99ce70da206a4bf34ebea0a7e6
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-09 13:41:49 +0700

    feat: implement getAllUsers

--- src/handlers/getAllUsers.ts
+++ src/handlers/getAllUsers.ts
@@ -2,12 +2,11 @@ import { ServerResponse } from 'http';
 import { sendJSONResponse } from '../utils/sendResponse';
 import { db } from '../db/InMemoryDB';
 
-export async function getAllUsers(res: ServerResponse): Promise<void> {
+export function getAllUsers(res: ServerResponse): void {
   try {
     const response = db.findAllUsers();
-    if (response) {
-      sendJSONResponse(res, 200, response);
-    }
+    if (response) throw new Error('No users found');
+    sendJSONResponse(res, 200, response);
   } catch (error) {
     sendJSONResponse(res, 500, { message: 'Internal Server Error' });
   }
