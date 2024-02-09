c44152a65df5ec5ec819edc72fecb97f5f8249c0
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-09 13:44:02 +0700

    feat: implement deleteUser

--- src/handlers/deleteUser.ts
+++ src/handlers/deleteUser.ts
@@ -2,10 +2,10 @@ import { ServerResponse } from 'http';
 import { sendJSONResponse } from '../utils/sendResponse';
 import { db } from '../db/InMemoryDB';
 
-export async function deleteUser(res: ServerResponse, id: string) {
+export function deleteUser(res: ServerResponse, id: string) {
   try {
-    const success = db.deleteUser(id);
-    sendJSONResponse(res, success ? 200 : 404, { message: success ? 'User deleted successfully' : 'User not found' });
+    const isDeleted = db.deleteUser(id);
+    sendJSONResponse(res, isDeleted ? 200 : 404, { message: isDeleted ? 'User deleted successfully' : 'User not found' });
   } catch (error) {
     sendJSONResponse(res, 500, { message: 'Internal Server Error' });
   }
