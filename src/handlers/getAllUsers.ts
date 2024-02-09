9d505a0e044e7c41fcc5c55adf74b2a26d1e6555
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-09 19:53:25 +0700

    fix: update implementation

--- src/handlers/getAllUsers.ts
+++ src/handlers/getAllUsers.ts
@@ -4,9 +4,6 @@ import { db } from '../db/InMemoryDB';
 
 export function getAllUsers(res: ServerResponse): void {
   const response = db.findAllUsers();
-  if (response.length !== 0) {
-    sendJSONResponse(res, 200, response);
-  } else {
-    sendJSONResponse(res, 200, {message: 'Users list is empty'});
-  }
+  sendJSONResponse(res, 200, response);
+  
 }
