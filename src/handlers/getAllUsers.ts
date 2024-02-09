1b3cef7005c659a90d2950bc1e9678edf9964c9d
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-09 15:40:23 +0700

    fix: fix typo

--- src/handlers/getAllUsers.ts
+++ src/handlers/getAllUsers.ts
@@ -4,7 +4,8 @@ import { db } from '../db/InMemoryDB';
 
 export function getAllUsers(res: ServerResponse): void {
   const response = db.findAllUsers();
-  if (response.length === 0) {
+  console.log(response);
+  if (response.length !== 0) {
     sendJSONResponse(res, 200, response);
   } else {
     sendJSONResponse(res, 200, {message: 'Users list is empty'});
