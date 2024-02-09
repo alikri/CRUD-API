472c3813db51182630919e95d4a30bd0a2624a25
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-09 15:44:06 +0700

    fix: update mock data to to pass through uuidv validation

--- src/mockData/usersDefaultData.ts
+++ src/mockData/usersDefaultData.ts
@@ -1,15 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export const data = [
   {
-    id: "abcdef123456789",
-    username: "john_doe",
+    id: uuidv4(),
+    username: 'john_doe',
     age: 30,
-    hobbies: ["reading", "gardening"]
+    hobbies: ['reading', 'gardening'],
   },
   {
-    id: "ghijkl987654321",
-    username: "jane_smith",
+    id: uuidv4(),
+    username: 'jane_smith',
     age: 25,
-    hobbies: ["painting"]
-  }
+    hobbies: ['painting'],
+  },
 ];
-
